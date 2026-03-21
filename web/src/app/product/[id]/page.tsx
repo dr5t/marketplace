7a6bbf7ac3e715363f3a08ef10b29c12a41b1123
!,Dr-D <tiwarishaurya395@gmail.com>
2026-03-21 13:20:22 +0530

    feat: Add `generateStaticParams` to pre-render product detail pages with static IDs.

--- web/src/app/product/[id]/page.tsx
+++ web/src/app/product/[id]/page.tsx
@@ -228,4 +228,15 @@ const ProductDetail = () => {
   );
 };
 
+export async function generateStaticParams() {
+  return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    { id: '6' },
+  ];
+}
+
 export default ProductDetail;
